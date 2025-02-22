4bd2c1b9647a2f6bf795659f69b661437b7258c4
Slava Kalmykov <slavafort35@gmail.com>
2025-02-22 15:01:02 +0200

    norm a fuel has been added into settings model

--- workfuel/models.py
+++ workfuel/models.py
@@ -50,20 +50,15 @@ class MovementTime(db.Model):
     workerspark_id = db.Column(db.Integer, db.ForeignKey('workparks.id'), nullable=False)
 
 
-class WorkParks(db.Model):
+class WorkPark(db.Model):
     __tablename__ = 'workparks'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    park_l = db.Column(db.Float, nullable=True)
-    park_g = db.Column(db.Float, nullable=True)
-    park_e = db.Column(db.Float, nullable=True)
-    park_z = db.Column(db.Float, nullable=True)
-    park_vm = db.Column(db.Float, nullable=True)
-    park_nijny = db.Column(db.Float, nullable=True)
-    park_vchd_3 = db.Column(db.Float, nullable=True)
-    park_tch_1 = db.Column(db.Float, nullable=True)
-    hot_state = db.Column(db.Float, nullable=True)
-    cool_state = db.Column(db.Float, nullable=True)
     locomotive_id = db.Column(db.Integer, db.ForeignKey('locomotives.id'), nullable=False)
+    park_name = db.Column(db.String(30), nullable=False)
+    work_hours = db.Column(db.Float, nullable=False, default=0)
+    hot_state = db.Column(db.Float, nullable=False, default=0)
+    cool_state = db.Column(db.Float, nullable=False, default=0)
+    locomotive = db.relationship('Locomotive', backref=db.backref('workparks', lazy=True))
 
 
 class ReserveRun(db.Model):
@@ -106,6 +101,14 @@ class Settings(db.Model):
     park_kaidakskaya_norm = db.Column(db.Float, nullable=False, default=13.5)
     park_nizhnedneprovsk_norm = db.Column(db.Float, nullable=False, default=14.5)
     park_pristan_norm = db.Column(db.Float, nullable=False, default=14.5)
+    park_lotsmanka_norm = db.Column(db.Float, nullable=False, default=12.0)
+    park_vstrechnyy_norm = db.Column(db.Float, nullable=False, default=13.0)
+    park_dn_gruzovoy_norm = db.Column(db.Float, nullable=False, default=12.0)
+    park_obvodnaya_norm = db.Column(db.Float, nullable=False, default=12.0)
+    park_lisky_norm = db.Column(db.Float, nullable=False, default=12.0)
+    park_privolnoe_norm = db.Column(db.Float, nullable=False, default=13.0)
+    park_rasnaya_norm = db.Column(db.Float, nullable=False, default=13.0)
+    park_suhachovka_norm = db.Column(db.Float, nullable=False, default=12.0)
     hot_state = db.Column(db.Integer, nullable=False, default=10)
     cool_state = db.Column(db.Integer, nullable=False, default=0)
 
